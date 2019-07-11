363e8fd4f8e7f149b63341542280fd01a068484b
zhou-hao <admin@hsweb.me>
2019-07-11 17:51:11 +0800

    增加prefix支持

--- lettuce-plus-spring/src/main/java/org/jetlinks/lettuce/spring/cache/LettuceCacheManager.java
+++ lettuce-plus-spring/src/main/java/org/jetlinks/lettuce/spring/cache/LettuceCacheManager.java
@@ -1,5 +1,7 @@
 package org.jetlinks.lettuce.spring.cache;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.jetlinks.lettuce.LettucePlus;
 import org.springframework.cache.Cache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
@@ -7,6 +9,10 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 public class LettuceCacheManager extends ConcurrentMapCacheManager {
 
     private LettucePlus plus;
+
+    @Setter
+    @Getter
+    private String cachePrefix = "spring-cache:";
 
     public LettuceCacheManager(LettucePlus plus) {
         this.plus = plus;
@@ -17,7 +23,7 @@ public class LettuceCacheManager extends ConcurrentMapCacheManager {
     protected Cache createConcurrentMapCache(String name) {
 
         return new LettuceCache(name,
-                plus.getLocalCacheMap(name),
+                plus.getLocalCacheMap(cachePrefix.concat(name)),
                 isAllowNullValues());
     }
 }
